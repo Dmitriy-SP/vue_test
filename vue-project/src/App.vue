df5358ce04b9a4de8ce6434ec1521cf7f6095ab2
dmitriy <dmitriychereha@gmail.com>
2024-02-29 19:47:38 +0300

    make first vue2 project

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,55 +1,93 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+<script>
+export default {
+  data() {
+    return {
+      city: "",
+      error: "",
+    };
+  },
+  computed: {
+    cityName() {
+      return `"${this.city}"`;
+    },
+  },
+  methods: {
+    getWeather() {
+      if (this.city.trim().length < 2) {
+        this.error = "Нужно название более 1 символа";
+        return false;
+      }
+      this.error = "";
+      return true;
+    },
+  },
+};
 </script>
 
 <template>
-  <div id="app">
-    <header>
-      <img
-        alt="Vue logo"
-        class="logo"
-        src="./assets/logo.svg"
-        width="125"
-        height="125"
-      />
-
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-      </div>
-    </header>
-
-    <main>
-      <TheWelcome />
-    </main>
+  <div class="wrapper">
+    <h1>Погодное приложение</h1>
+    <p>{{ city ? `Узнать погоду в ${cityName}` : "Введите город" }}</p>
+    <input type="text" v-model="city" placeholder="Введите город" />
+    <button v-if="city" v-on:click="getWeather()">Узнать погоду</button>
+    <button disabled v-else>Введите город</button>
+    <p class="error">{{ error }}</p>
   </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.error {
+  color: #d03939;
+}
+.wrapper {
+  width: 900px;
+  height: 500px;
+  border-radius: 50px;
+  background: #1f0f24;
+  text-align: center;
+  color: #fff;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.wrapper h1 {
+  margin-top: 50px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.wrapper p {
+  margin-top: 20px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.wrapper input {
+  margin-top: 30px;
+  background: transparent;
+  border: 0;
+  border-bottom: 2px solid #110813;
+  color: #fcfcfc;
+  font-size: 14px;
+  padding: 5px 8px;
+  outline: none;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.wrapper input:focus {
+  border-bottom-color: #6e2d7d;
+}
+
+.wrapper button {
+  background: #e3bc4d;
+  color: #fff;
+  border-radius: 10px;
+  border: 2px solid #b99935;
+  padding: 10px 15px;
+  margin-left: 20px;
+  cursor: pointer;
+}
+
+.wrapper button:hover:enabled {
+  transition: transform 500ms ease;
+  transform: scale(1.1) translateY(-5px);
+}
+
+.wrapper button:disabled {
+  background: #746027;
+  cursor: not-allowed;
 }
 </style>
